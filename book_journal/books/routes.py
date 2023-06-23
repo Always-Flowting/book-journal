0c467d774baf7bce940a15a6b8570b6a686e9624
Always-Flowting <1stvaulthunter@gmail.com>
2023-06-22 21:25:20 -0400

    Added model for books

--- book_journal/books/routes.py
+++ book_journal/books/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 books = Blueprint('books', __name__)
 
@@ -6,4 +6,5 @@ books = Blueprint('books', __name__)
 @books.route('/book/search', methods=['POST'])
 def search_books():
     books = ['The Great Gatsby', 'To Kill a Mockingbird', '1984', 'The Catcher in the Rye']
-    return jsonify({'books': books})
+    data = request.get_json()
+    return jsonify({'books': [data.get('search_term')]})
